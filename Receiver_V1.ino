70c4eb80c8cff76b424aa6ee3954caa3762a74d8
Aqtion <45376658+Aqtion@users.noreply.github.com>
2022-03-18 14:14:54 -0700

    Update Receiver_V1.ino

--- Receiver_V1.ino
+++ Receiver_V1.ino
@@ -15,12 +15,12 @@ RF24 radio(7, 8); // CE, CSN
 const byte address[1] = "00002"; //radio 'channel'
 boolean button_state = 0;
 int motor_pin = 2;
-int button_value1 = 0; 
-int button_value2 = 0; 
+int button_value1 = 0; //store button value given by first transmitter
+int button_value2 = 0; //store button value given by second transmitter
 int led_unavailable = 5;
-float data[4];
-float data1;
-float data2;
+float data[4]; //data taken from 2 transmitters
+float data1; //store data from first transmitter
+float data2; //store data from second transmitter
 int csn = 9;
 int ce = 7;
 void setup() {
@@ -54,24 +54,27 @@ digitalWrite(motor_pin, LOW);
 void loop() {
 if (radio.available()){              //Looking for the data.
     //radio.read(&button_value, sizeof(button_value));    //Reading the data
-    if(data[0]==501) {
-       data1 = data[1];
-       button_value1 = data1;
+    if(data[0]==501) { //if checking to see if data was sent from first transmitter
+       data1 = data[1]; //set data from first transmitter to data given
+       button_value1 = data1; //set button value to data transmitted
     }
-    else if(data[0]==601) {
-       data2 = data[1];
-       button_value2 = data2;
+    else if(data[0]==601) { //if checking to see if data was sent from second transmitter
+       data2 = data[1]; //set data from second transmitter to data given
+       button_value2 = data2; //set button value to data transmitted
     }
     //Serial.println(button_value);
     Serial.println(button_value);
+    //if checking both button values since either can be 1 for motor to buzz
     if(button_value1 == 1 || button_value2==1){
       digitalWrite(motor_pin, HIGH);
       delay(1000);
       digitalWrite(motor_pin,LOW);
+      //set both to 0 to avoid overloading and delay
       button_value1 = 0;
       button_value2 = 0;
       Serial.println(button_value);
     }
+    //weird if covered for both values
     else if(button_value1 == 28505 || button_value2 == 28505){
     }
     else {
