dd0bc2f47c7915032f72f5b989bd4a65ca15075d
Aqtion <45376658+Aqtion@users.noreply.github.com>
2022-07-08 22:11:02 +0530

    Update Inventeams_Feather_ReceiverDelay_Code.ino

--- Inventeams_Feather_ReceiverDelay_Code.ino
+++ Inventeams_Feather_ReceiverDelay_Code.ino
@@ -76,7 +76,7 @@ void loop() {
     if (rf69.recv(&transmittertime, &len)) {
       if (!len) return;
       if(transmittertime!=0) {
-        Serial.print( ((int)(transmittertime) - receivertime) + " milliseconds");
+        Serial.print( (receivertime) - (int)(transmittertime)) + " milliseconds");
       }
       digitalWrite(led_unavailable, LOW);
      // Serial.print("Received [");
