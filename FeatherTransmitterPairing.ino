0622548ab9dd77bf8054e62beb30d2e856c6ead1
Keshav Badrinath <45376658+Aqtion@users.noreply.github.com>
2022-11-28 16:00:44 -0800

    iterative questioning

--- FeatherTransmitterPairing.ino
+++ FeatherTransmitterPairing.ino
@@ -2,25 +2,70 @@
 #include <RH_RF69.h>
 
 #define RF69_FREQ 915.0
-uint8_t NEW_FREQ=2;
+
+#define VBATPIN A7
+float vbatm = 0;
+uint8_t iter = 0;
+uint8_t last_state = 0;
+#define THRESHOLD_VOLTAGE 3.7
+
+#define RED_PIN A1
+#define GREEN_PIN A2
+#define BLUE_PIN A3
+
+#define BUTTON_PIN 12
 
 #if defined(ADAFRUIT_FEATHER_M0) || defined(ADAFRUIT_FEATHER_M0_EXPRESS) || defined(ARDUINO_SAMD_FEATHER_M0)
-  #define RFM69_CS      8
-  #define RFM69_INT     3
-  #define RFM69_RST     4
-  #define LED           13
+#define RFM69_CS      8
+#define RFM69_INT     3
+#define RFM69_RST     4
 #endif
 
 
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
 uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
 
-void setup() 
+typedef struct {
+  int r;
+  int g;
+  int b;
+} Color;
+
+namespace Colors {
+Color RED = {255, 0, 0};
+Color GREEN = {0, 255, 0};
+}
+
+void rgb_color(Color color) {
+  analogWrite(RED_PIN, color.r);
+  analogWrite(GREEN_PIN, color.g);
+  analogWrite(BLUE_PIN, color.b);
+}
+
+float get_voltage() {
+  float measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2;
+  measuredvbat *= 3.3;
+  measuredvbat /= 1024;
+  return measuredvbat;
+}
+
+uint8_t debounce() {
+    uint8_t debounced_state = 0;
+    debounced_state = (debounced_state << 1) | digitalRead(BUTTON_PIN) | 0xfe00;
+    return debounced_state;
+}
+
+void setup()
 {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   Serial.begin(115200);
 
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
 
-  pinMode(LED, OUTPUT);     
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RFM69_RST, OUTPUT);
   digitalWrite(RFM69_RST, LOW);
 
@@ -28,32 +73,60 @@ void setup()
   delay(10);
   digitalWrite(RFM69_RST, LOW);
   delay(10);
-  
+
   if (!rf69.init()) {
     Serial.println("RFM69 radio init failed");
     while (1);
   }
- 
+
   if (!rf69.setFrequency(RF69_FREQ)) {
     Serial.println("setFrequency failed");
   }
 
   rf69.setTxPower(20, true);
 
-  uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 ,
-                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-  
+  uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08
+                  };
+
   rf69.setEncryptionKey(key);
-  
-  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  Serial.print("Broadcasting offset [");
-  Serial.print(NEW_FREQ);
-  Serial.print("] on ");
-  Serial.print(RF69_FREQ);
-  Serial.println("MHz ");
-  
-  rf69.send(&NEW_FREQ, sizeof(NEW_FREQ));
+  vbatm = get_voltage();
+  Serial.print("Voltage: ");
+  Serial.println(vbatm);
+
+  rgb_color(
+    vbatm < THRESHOLD_VOLTAGE ?
+    Colors::RED
+    :
+    Colors::GREEN
+  );
+
+  uint8_t button_state = debounce();
+  buf[0] = button_state;
+  iter = iter+(last_state==button_state)?0:1;
+  buf[1] = iter;
+  rf69.send(buf, sizeof(buf));
+  delay(10);
+  rf69.send(buf, sizeof(buf));
+  delay(10);
+  rf69.send(buf, sizeof(buf));
+  last_state = button_state;
 }
+
+//string getChipID() {
+//  volatile uint32_t val1, val2, val3, val4;
+//  volatile uint32_t *ptr1 = (volatile uint32_t *)0x0080A00C;
+//  val1 = *ptr1;
+//  volatile uint32_t *ptr = (volatile uint32_t *)0x0080A040;
+//  val2 = *ptr;
+//  ptr++;
+//  val3 = *ptr;
+//  ptr++;
+//  val4 = *ptr;
+//
+//  string concatenate = val1 + "" + val2 + "" + val3 + "" + val4;
+//  return concatenate;
+//}
