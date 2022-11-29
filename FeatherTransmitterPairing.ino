400b49dbfe92a24053162e14c8dc44a4c4d63760
wlmchen <65417452+wlmchen@users.noreply.github.com>
2022-11-29 14:57:03 -0800

    map multiple recievers thingy
    i hope this works.
    trust

--- FeatherTransmitterPairing.ino
+++ FeatherTransmitterPairing.ino
@@ -1,13 +1,16 @@
 #include <SPI.h>
 #include <RH_RF69.h>
 
+#include <array>
+
 #define RF69_FREQ 915.0
 
 #define VBATPIN A7
 float vbatm = 0;
-uint8_t iter = 0;
-uint8_t last_state = 0;
+
 #define THRESHOLD_VOLTAGE 3.7
+
+#define UUID_LEN 16
 
 #define RED_PIN A1
 #define GREEN_PIN A2
@@ -20,6 +23,7 @@ uint8_t last_state = 0;
 #define RFM69_INT     3
 #define RFM69_RST     4
 #endif
+
 
 
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
@@ -36,6 +40,36 @@ Color RED = {255, 0, 0};
 Color GREEN = {0, 255, 0};
 }
 
+typedef std::array<uint8_t, UUID_LEN> uuid;
+
+uuid getChipID() {
+  uuid chipId;
+
+  volatile uint32_t val1, val2, val3, val4;
+  volatile uint32_t *ptr1 = (volatile uint32_t *)0x0080A00C;
+  val1 = *ptr1;
+  volatile uint32_t *ptr = (volatile uint32_t *)0x0080A040;
+  val2 = *ptr;
+  ptr++;
+  val3 = *ptr;
+  ptr++;
+  val4 = *ptr;
+  uint32_t orig[4] = {val1, val2, val3, val4};
+
+  //https://stackoverflow.com/questions/6499183/converting-a-uint32-value-into-a-uint8-array4
+  //  no clue if this works but yeah
+  for (int i = 0; i < 4; i++) {
+    uint8_t *origp = (uint8_t *)&orig;
+    for (int j = 0; j < 4; j++) {
+      chipId[4 * i + j] = origp[j];
+    }
+  }
+  return chipId;
+}
+
+uuid UUID = getChipID();
+
+
 void rgb_color(Color color) {
   analogWrite(RED_PIN, color.r);
   analogWrite(GREEN_PIN, color.g);
@@ -50,10 +84,16 @@ float get_voltage() {
   return measuredvbat;
 }
 
-uint8_t debounce() {
-    uint8_t debounced_state = 0;
-    debounced_state = (debounced_state << 1) | digitalRead(BUTTON_PIN) | 0xfe00;
-    return debounced_state;
+boolean debounce() {
+  uint8_t debounced_state = 0;
+  debounced_state = (debounced_state << 1) | digitalRead(BUTTON_PIN) | 0xfe00;
+  return debounced_state;
+}
+
+void set_data(uint8_t data[], int len) {
+  for (int i = 0; i < len; i++) {
+    buf[UUID_LEN + i + 1] = data[i];
+  }
 }
 
 void setup()
@@ -90,6 +130,11 @@ void setup()
                   };
 
   rf69.setEncryptionKey(key);
+
+  for (int i = 0; i < UUID_LEN; i++) {
+    //    Load UUID into buf
+    buf[i] = UUID[i];
+  }
 }
 
 void loop() {
@@ -105,28 +150,11 @@ void loop() {
   );
 
   uint8_t button_state = debounce();
-  buf[0] = button_state;
-  iter = iter+(last_state==button_state)?0:1;
-  buf[1] = iter;
+  Serial.println("Button State:");
+  Serial.println(button_state);
+  uint8_t data[] = {button_state};
+  set_data(data, 1);
+
   rf69.send(buf, sizeof(buf));
-  delay(10);
-  rf69.send(buf, sizeof(buf));
-  delay(10);
-  rf69.send(buf, sizeof(buf));
-  last_state = button_state;
 }
 
-//string getChipID() {
-//  volatile uint32_t val1, val2, val3, val4;
-//  volatile uint32_t *ptr1 = (volatile uint32_t *)0x0080A00C;
-//  val1 = *ptr1;
-//  volatile uint32_t *ptr = (volatile uint32_t *)0x0080A040;
-//  val2 = *ptr;
-//  ptr++;
-//  val3 = *ptr;
-//  ptr++;
-//  val4 = *ptr;
-//
-//  string concatenate = val1 + "" + val2 + "" + val3 + "" + val4;
-//  return concatenate;
-//}
